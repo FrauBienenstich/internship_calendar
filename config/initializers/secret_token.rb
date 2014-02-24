778877f87f179edeb5e6a6eb1062c21559d7a0ed
Susanne Dewein <susanne.dewein@gmail.com>
2014-02-24 11:18:46 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InternshipCalender::Application.config.secret_token = '518a177ede5bec8178359359b05f2adc60b31491208e128e45b061a7214a66669b185781edc921c0df6a8346fd08d30aea140091b7751d5793a4c67a3dc0312e'
