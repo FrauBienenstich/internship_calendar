46413805ada29874144a4dd5bc0670fc141e762e
Susanne Dewein <susanne.dewein@gmail.com>
2014-02-25 18:04:17 +0100

    Mess around with show view, including tests TBC

--- spec/features/host_offers_slot.rb
+++ spec/features/host_offers_slot.rb
@@ -4,17 +4,25 @@ require_relative '../../lib/seeds/create_new_day.rb'
 feature 'Host offers slot' do
 
   scenario 'offers slot for the first time' do
-    create_new_day
     date = Date.new(2014,04,21)
     create_new_day(date)
 
 
     visit 'days/current'
 
+    within('td') do
+      fill_in "Email", with: "a@b.de"
+    end
+    # fill_in :placeholder => "Name", with: "Susanne"
+
     save_and_open_page
     expect(page).to have_content(date)
     expect(page).to have_content("9am - 10am")
     expect(page).to have_content("10am - 12 am")
+
+    expect(page).to have_content("a@b.de")
+    # expect(page).to have_content("Susanne")
+
 
   end
 
