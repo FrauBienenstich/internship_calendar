df1aa39145b045ac4add94b2268ac4176e38f15e
Susanne Dewein <susanne.dewein@gmail.com>
2014-02-25 12:16:24 +0100

    Fix acceptance tests

--- spec/features/host_offers_slot.rb
+++ spec/features/host_offers_slot.rb
@@ -5,10 +5,11 @@ feature 'Host offers slot' do
   scenario 'offers slot for the first time' do
     today = Date.today
     day = Day.create!(:date => today)
-    Slot.create!(:name => "9am - 10am", :day => day)
-    Slot.create!(:name => "10am - 12 am", :day => day)
+    Slot.create!(:name => "9am - 10am", :day_id => day)
+    Slot.create!(:name => "10am - 12 am", :day_id => day)
 
-    visit '/'
+    visit 'days/current'
+
     expect(page).to have_content(today)
     expect(page).to have_content("9am - 10am")
 
