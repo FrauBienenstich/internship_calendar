3fa3be1cd54d7dbc1fab7de1a02d24d11cf4b56f
Susanne Dewein <susanne.dewein@gmail.com>
2014-02-25 12:13:17 +0100

    Fix attributes
    Atrributes are foreign keys now

--- db/migrate/20140224124633_create_internships.rb
+++ db/migrate/20140224124633_create_internships.rb
@@ -1,8 +1,8 @@
 class CreateInternships < ActiveRecord::Migration
   def change
     create_table :internships do |t|
-      t.string :host
-      t.string :intern
+      t.integer :host_id
+      t.integer :intern_id
 
       t.timestamps
     end
