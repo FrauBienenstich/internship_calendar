3583e98c536ddbb746ae235bc0e0a5dfd78893f1
Susanne Dewein <susanne.dewein@gmail.com>
2014-02-25 16:06:42 +0100

    Fix instance variable thingy again
    instead of using an instance variable i should rather hand it on as an argument to the method

--- lib/seeds/create_new_day.rb
+++ lib/seeds/create_new_day.rb
@@ -1,6 +1,5 @@
-def create_new_day
-  @today = Date.new(2014,04,21)
-  day = Day.create!(:date => @today)
+def create_new_day(date)
+  day = Day.create!(:date => date)
   Slot.create!(:name => "9am - 10am", :day_id => day.id)
   Slot.create!(:name => "10am - 12 am", :day_id => day.id)
 end
--- spec/features/host_offers_slot.rb
+++ spec/features/host_offers_slot.rb
@@ -5,11 +5,14 @@ feature 'Host offers slot' do
 
   scenario 'offers slot for the first time' do
     create_new_day
+    date = Date.new(2014,04,21)
+    create_new_day(date)
+
 
     visit 'days/current'
 
     save_and_open_page
-    expect(page).to have_content(@today)
+    expect(page).to have_content(date)
     expect(page).to have_content("9am - 10am")
     expect(page).to have_content("10am - 12 am")
 
