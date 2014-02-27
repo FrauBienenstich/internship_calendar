0d107c4f47079168b7d5589923077405ee0e5aaa
Susanne Dewein <susanne.dewein@gmail.com>
2014-02-27 16:47:45 +0100

    validate email format

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,9 @@
 class Person < ActiveRecord::Base
 
+  validates :email, uniqueness: true
   has_many :lectureships, :class_name => "Internship"
   has_many :internships, :class_name => "Internship"
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
+
 
 end
