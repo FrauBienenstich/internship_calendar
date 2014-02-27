ae13dfc2d4d0e0f39b85932ca6c807d5dd2f4445
Susanne Dewein <susanne.dewein@gmail.com>
2014-02-27 15:30:59 +0100

    Add validation for email

--- app/models/internship.rb
+++ app/models/internship.rb
@@ -5,6 +5,8 @@ class Internship < ActiveRecord::Base
   validates :description, presence: true
   validates :host, uniqueness: true
 
+  validates :host, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
+
   belongs_to :slot
   belongs_to :host, :class_name => "Person"
   belongs_to :intern, :class_name => "Person"
