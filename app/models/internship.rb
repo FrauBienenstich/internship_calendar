45f5d8a0bf8f23311dd8fad21b52055f56d16d05
Susanne Dewein <susanne.dewein@gmail.com>
2014-02-27 16:48:04 +0100

    remove comment

--- app/models/internship.rb
+++ app/models/internship.rb
@@ -1,11 +1,8 @@
 class Internship < ActiveRecord::Base
 
-  validates :host, presence: true #wieso kann name immernoch doppelt sein??
+  validates :host, presence: true
   validates :slot_id, presence: true
   validates :description, presence: true
-  validates :host, uniqueness: true
-
-  validates :host, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
 
   belongs_to :slot
   belongs_to :host, :class_name => "Person"
