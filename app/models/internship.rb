70b8f201514974ec07a4faea91a9cbfa24a6826a
Susanne Dewein <susanne.dewein@gmail.com>
2014-02-27 13:12:40 +0100

    Add internship validations

--- app/models/internship.rb
+++ app/models/internship.rb
@@ -1,5 +1,13 @@
 class Internship < ActiveRecord::Base
+
+  validates :host, presence: true
+  validates :slot_id, presence: true
+  validates :description, presence: true
+
   belongs_to :slot
   belongs_to :host, :class_name => "Person"
   belongs_to :intern, :class_name => "Person"
+
+
+
 end
