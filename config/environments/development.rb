0218a51bc3333b2fb0232889520cb52733aa8d45
Susanne Dewein <susanne.dewein@gmail.com>
2014-02-28 15:25:30 +0100

    Set up SMTP settings
    not sure those ENV variables were necessary

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,16 @@ InternshipCalender::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => ENV["SMTP_ADDRESS"],
+  :authentication       => 'plain',
+  :port                 => 25,
+  :domain               => ENV["SMTP_DOMAIN"],
+  :enable_starttls_auto => true  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
