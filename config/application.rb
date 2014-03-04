1fb4f1f955b5f75e02b6e8657433cb288401def9
Susanne Dewein <susanne.dewein@gmail.com>
2014-03-04 17:11:12 +0100

    Get rid of deprecation warnings

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,8 @@ module InternshipCalender
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    I18n.enforce_available_locales = false
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
@@ -53,7 +55,7 @@ module InternshipCalender
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    config.active_record.whitelist_attributes = false
 
     # Enable the asset pipeline
     config.assets.enabled = true
