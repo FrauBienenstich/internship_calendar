eede980f92fbd46b274d3229ce20af3158cec482
Susanne Dewein <susanne.dewein@gmail.com>
2014-03-06 10:49:12 +0100

    Add factories

---
+++ spec/support/factories.rb
@@ -0,0 +1,21 @@
+FactoryGirl.define do
+
+  factory :internship do
+
+  end
+
+  factory :host do
+
+  end
+
+  factory :intern do
+
+  end
+
+  factory :slot do
+
+    name '1 - 2 pm'
+    #day_id '1'
+  end
+  
+end
