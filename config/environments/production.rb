6c66a5b84dd79f1179424ee0ab8e7eaf64e7345a
Susanne Dewein <susanne.dewein@gmail.com>
2014-03-20 16:55:50 +0100

    Use send grid for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,19 @@ InternshipCalender::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.raise_delivery_errors = true
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => 'smtp.sendgrid.net',
+  :port                 => '587',
+  :authentication       => 'plain',
+  :user_name            => ENV['SENDGRID_USERNAME'],
+  :password             => ENV['SENDGRID_PASSWORD'],
+  :domain               => 'heroku.com',
+  :enable_starttls_auto => true  }
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
