52c978bd77f1ee9ac9f7288af7b9da6f991aa5f3
Susanne Dewein <susanne.dewein@gmail.com>
2014-04-02 14:13:25 +0200

    Fix seeds for rake task

--- lib/seeds/create_new_day.rb
+++ lib/seeds/create_new_day.rb
@@ -1,6 +1,4 @@
 def create_new_day(date)
   day = Day.create!(:date => date)
   dt = date.to_datetime
-  Slot.create!(:start_time => (dt + 9.hours), :end_time => (dt + 10.hours), :day_id => day.id)
-  Slot.create!(:start_time => (dt + 11.hours), :end_time => (dt + 12.hours), :day_id => day.id)
 end
