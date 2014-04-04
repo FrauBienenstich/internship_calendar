d1a34ddaeef60dd15b7ed6fde09d733b8b61120e
Susanne Dewein <susanne.dewein@gmail.com>
2014-04-04 11:35:11 +0200

    Fix migrations so that it works with postgres

--- db/migrate/20140331092932_change_start_time_format_in_internships.rb
+++ db/migrate/20140331092932_change_start_time_format_in_internships.rb
@@ -1,5 +1,14 @@
 class ChangeStartTimeFormatInInternships < ActiveRecord::Migration
-  def change
-    change_column :internships, :start_time, :datetime
+  class Internship < ActiveRecord::Base 
+  end
+
+  def up
+    Internship.update_all("start_time = NULL")
+    change_column :internships, :start_time, :datetime, :null => false
+    Internship.update_all("start_time = #{DateTime.now.to_s(:db)}")
+  end
+
+  def down
+    change_column :internships, :start_time, :time
   end
 end
--- db/migrate/20140331093546_change_end_time_format_in_internships.rb
+++ db/migrate/20140331093546_change_end_time_format_in_internships.rb
@@ -1,5 +1,13 @@
 class ChangeEndTimeFormatInInternships < ActiveRecord::Migration
-  def change
-    change_column :internships, :end_time, :datetime
+  class Internship < ActiveRecord::Base end
+
+  def up
+    Internship.update_all("end_time = NULL")
+    change_column :internships, :end_time, :datetime, :null => false
+    Internship.update_all("end_time = #{(DateTime.now + 1.hour).to_s(:db)}")
+  end
+
+  def down
+    change_column :internships, :end_time, :time
   end
 end
