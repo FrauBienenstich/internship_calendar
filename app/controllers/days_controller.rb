f2c362666e1f29da48e4d78ae7381b0d5f5ecc18
Susanne Dewein <susanne.dewein@gmail.com>
2014-04-07 11:21:42 +0200

    Add new_day form

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -4,6 +4,24 @@ class DaysController < ApplicationController
     @start_page = true
   end
 
+  def create
+
+    date = Date.new(params["day"]["date(1i)"].to_i, params["day"]["date(2i)"].to_i, params["day"]["date(3i)"].to_i)
+
+    @day = Day.new(date: date)
+
+    if @day.save
+      redirect_to days_path, notice: "You created a new internship day!"
+    else
+      redirect_to days_path, :flash => { :error => "Your new day could not be saved"}
+    end
+  end
+
+  def new
+    @day = Day.new
+    render :layout => false
+  end
+
   def show
     @day = Day.find(params[:id])
     @internships = @day.internships.order('start_time ASC')
