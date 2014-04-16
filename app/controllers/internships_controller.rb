1647b24d3b0a2925cddcdf77cd679a410982e4e6
Susanne Dewein <susanne.dewein@gmail.com>
2014-04-16 12:12:18 +0200

    Adapting the start_time/end_time params in #create
    the format is now alright from the beginning, no need to change it. so i got rid of #make_time and #make_time_string

--- app/controllers/internships_controller.rb
+++ app/controllers/internships_controller.rb
@@ -7,30 +7,11 @@ class InternshipsController < ApplicationController
 
     host = Person.find_or_new(params[:email], params[:name])
 
-    # start_time = Date.new *new_start_time_from_hash(params[:internship])
-    # end_time = Date.new *new_end_time_from_hash(params[:internship])
-
-    start_time = make_time("start_time")
-    end_time = make_time("end_time")
-    # start_time = DateTime.new(params["internship"]["start_time(1i)"].to_i, 
-    #                     params["internship"]["start_time(2i)"].to_i,
-    #                     params["internship"]["start_time(3i)"].to_i,
-    #                     params["internship"]["start_time(4i)"].to_i,
-    #                     params["internship"]["start_time(5i)"].to_i)
-    # instead of getting day and month through params i could also access internship.day.date etc
-
-    # end_time = DateTime.new(params["internship"]["end_time(1i)"].to_i, 
-    #                     params["internship"]["end_time(2i)"].to_i,
-    #                     params["internship"]["end_time(3i)"].to_i,
-    #                     params["internship"]["end_time(4i)"].to_i,
-    #                     params["internship"]["end_time(5i)"].to_i)
-    #TODO Refactor!
-    
     internship = Internship.new(:description => params[:description], 
                                 :day_id => params[:day_id],
                                 :host => host,
-                                :start_time => start_time,
-                                :end_time => end_time)
+                                :start_time => params[:internship][:start_time],
+                                :end_time => params[:internship][:end_time])
 
     if host && host.save && internship && internship.save
       PersonMailer.confirmation_mail(internship).deliver
@@ -42,8 +23,6 @@ class InternshipsController < ApplicationController
   end
 
   def new
-    puts params
-    #@slot = Slot.find_by(id: params[:slot_id])
     @day = Day.find_by(id: params[:day_id])
 
     @internship = Internship.new
@@ -91,35 +70,6 @@ class InternshipsController < ApplicationController
 
     PersonMailer.delete_internship_mail(@internship).deliver
     redirect_to day_path(@internship.day), notice: "You successfully deleted an internship"
-  end
-
-  private
-
-  def make_time(whut)
-    begin
-      ts = make_time_string(whut)
-      puts "#{whut} -> #{ts}"
-      puts "TS #{ts}" #is empty, WHY???
-      return Time.zone.parse(ts)
-    rescue
-      nil
-    end
-  end
-
-  def make_time_string(whut)
-    puts "PARAMS #{params}"
-    y = params["internship"]["#{whut}(1i)"]
-    m = params["internship"]["#{whut}(2i)"]
-    d = params["internship"]["#{whut}(3i)"]
-    hh = params["internship"]["#{whut}(4i)"]
-    mm = params["internship"]["#{whut}(5i)"]
-
-    puts "STRING #{y}-#{pad(m)}-#{pad(d)} #{pad(hh)}:#{pad(mm)}"
-    "#{y}-#{pad(m)}-#{pad(d)} #{pad(hh)}:#{pad(mm)}"
-  end
-
-  def pad(num, len = 2)
-    num.to_s.rjust(len, '0')
   end
 
 end
