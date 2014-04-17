2a5d1260db6fde8c4046a93b924286195cea2676
Susanne Dewein <susanne.dewein@gmail.com>
2014-04-17 17:49:19 +0200

    Add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 InternshipCalender::Application.routes.draw do
 
-  root :to => 'days#index'
+  root :to => 'days#welcome'
   resources :internships do
     member do
       get :sign_up_form #generiert path und url helper
