d88eedc19c58e7995b34da55adb5f8c56826cfa0
Susanne Dewein <susanne.dewein@gmail.com>
2014-04-29 17:39:57 +0200

    Remove unused stuff from people controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,14 +1,3 @@
 class PeopleController < ApplicationController
 
-  def create
-    @person = Person.new
-
-    if @person.save
-      redirect_to :back, notice: 'Person was successfully created.'
-    else
-      render action: 'new'
-    end
-  end
 end
-
-#what exactly do i create??
