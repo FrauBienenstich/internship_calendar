c8e22c8bf339199813bc5bf3a776cdf5c503e8f2
Susanne Dewein <susanne.dewein@gmail.com>
2014-05-07 17:13:44 +0200

    Add nugg.ad fonts
    they are under assets/fonts

--- config/application.rb
+++ config/application.rb
@@ -63,5 +63,7 @@ module InternshipCalender
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
