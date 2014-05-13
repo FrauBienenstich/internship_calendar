dea26da0b6ef65c0946e8cf509b597c3495e1804
Susanne Dewein <susanne.dewein@gmail.com>
2014-05-13 12:25:41 +0200

    Add a couple of validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,8 @@
 class Person < ActiveRecord::Base
 
   validates :email, uniqueness: true
+  validates :email, presence: true
+  validates :name, presence: true
   has_many :lectureships, :class_name => "Internship"
   has_many :internships, :class_name => "Internship"
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
