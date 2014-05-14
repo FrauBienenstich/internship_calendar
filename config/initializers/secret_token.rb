1d131a7380083a9c57e8cf82f843ed98c82322b0
Susanne Dewein <susanne.dewein@gmail.com>
2014-05-14 10:10:04 +0200

    Get rid of deprecation warnings

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-InternshipCalender::Application.config.secret_token = '518a177ede5bec8178359359b05f2adc60b31491208e128e45b061a7214a66669b185781edc921c0df6a8346fd08d30aea140091b7751d5793a4c67a3dc0312e'
+
+InternshipCalender::Application.config.secret_key_base = '52a471c09d97ab6b879cfebbbd2369e1cb473a0ca38db88c2a5e9485c3cc1e7a69a03cc75bcaa93fd72b05a9725fcd6edca43b97c84ef949df8e70a13f6d6bc6'
